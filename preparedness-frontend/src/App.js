553a16b048c6ef8271d8bc1955418c86773a7aee
LouisBrizuela <louisbrizz@gmail.com>
2023-10-24 13:24:11 -0700

    finished

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,22 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+    const [personName, setPersonName] = useState("") 
+    const namePassed = (nameOfPerson) => { 
+      setPersonName(nameOfPerson.target.value) 
+  }
+  const reset = () => {
+    setPersonName("") 
+  }
+
   return (
-    <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
+    <body><div className="entire-content">
+      <h1 className="header">Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+          <Label className="label-text" for="name">Enter your name: </Label>
+          <Input id="inputfield" value={personName} onChange={namePassed} />
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <div className="buttons">
+          <Button id="reset-button" onClick={reset}>Reset</Button> 
+          <ModalComponent personName={personName} /> 
+        </div>
       </div>
     </div>
+    </body>
   )
 }
 
